d3f8f00b131d35797099de2cdee75479fd44c30e
Drup2021 <drupacharya@gmail.com>
2024-04-14 21:02:56 +0530

    last commit

--- app.py
+++ app.py
@@ -52,3 +52,6 @@ def return_file():
 # def display_video(filename):
 # 	#print('display_video filename: ' + filename)
 # 	return redirect(url_for('static/video_1.mp4', code=200))
+    
+if __name__ == "__main__":
+    app.run(debug=False,host='0.0.0.0')
